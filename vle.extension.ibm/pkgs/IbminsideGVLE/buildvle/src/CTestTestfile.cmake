b57da3c401bb73e70d27fd01ebffef3205e3748f
Geneviève Cirera <gcirera@eriste.toulouse.inra.fr>
2014-08-11 10:50:11 +0200

    first clean package, add example

--- vle.extension.ibm/pkgs/IbminsideGVLE/buildvle/src/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/gcicera/ibm/vle.extension.ibm/pkgs/IbminsideGVLE/src
-# Build directory: /home/gcicera/ibm/vle.extension.ibm/pkgs/IbminsideGVLE/buildvle/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-SUBDIRS(vle)
--- vle.extension.ibm/pkgs/IbminsideGVLE/buildvle/src/vle/gvle/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/gcicera/ibm/vle.extension.ibm/pkgs/IbminsideGVLE/src/vle/gvle
-# Build directory: /home/gcicera/ibm/vle.extension.ibm/pkgs/IbminsideGVLE/buildvle/src/vle/gvle
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-SUBDIRS(global)
